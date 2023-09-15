ddecc11ed6553e98a07e4619f521fbd2ae852700
kumarkirtimanraj <145006370+kumarkirtimanraj@users.noreply.github.com>
2023-09-15 12:58:34 +0530

    Create example.js

---
+++ example.js
@@ -0,0 +1,18 @@
+// This is a sample JavaScript file with linting issues
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log("The result is: " + add(1, 2));
+
+// Unused variable
+const unusedVar = "This variable is not used";
+
+// Missing semicolon
+const missingSemicolon = "This line should end with a semicolon"
+
+// Indentation error
+function indentationError() {
+return "Indentation is incorrect";
+}
